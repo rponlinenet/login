46ff7f88175e286005d1ac5357a0c0912072372c
rp <123549390+rponlinenet@users.noreply.github.com>
2023-10-09 01:42:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,6 @@ let input1=document.getElementById("input1")
 let input2=document.getElementById("input2")
 let submit=document.getElementById("submit")
 
-input1Value=input1.value
-input2Value=input2.value
 function save(){
 
 
