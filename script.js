28c6a2b1d2ba5c9598cf6f7a9edbad550bbad0b6
rp <123549390+rponlinenet@users.noreply.github.com>
2023-10-09 01:40:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 let input1=document.getElementById("input1")
 let input2=document.getElementById("input2")
 let submit=document.getElementById("submit")
-let data=[]
+
 input1Value=input1.value
 input2Value=input2.value
 function save(){
 
-data.push()
+
 localStorage.setItem(input1.value,input2.value)
 
 }
